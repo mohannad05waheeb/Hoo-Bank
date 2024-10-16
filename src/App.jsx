6585e54346a6c8b4fbd5d26edfe8943a0dd35328
mohannad05waheeb <mohannadwaheeb05@gmail.com>
2024-10-16 14:29:23 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import Appliction from "./Appliction";
+const App = () => {
+  return (
+    <Appliction /> 
+  );  
+};   
+export default App;
---
+++ src/components/index.jsx
@@ -0,0 +1,22 @@
+import Navbar from "./Navbar";  
+import Hero from "./Hero"; 
+import Billing from './Billing';  
+import Business from './Business';    
+import CardDeal from './cardDeal';     
+import Clients from './Clients';  
+import CTA from './CTA';  
+import Footer from './Footer';  
+import Stats from './Stats';  
+import Testimonials from './Testimonials';  
+export {
+    Navbar,
+    Hero,
+    Billing,
+    Business,
+    CardDeal,
+    Clients,
+    CTA,
+    Footer,
+    Stats,
+    Testimonials,
+}; 
